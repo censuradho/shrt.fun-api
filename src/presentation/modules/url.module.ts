cefd11a6e7671a80375a3ed96221ca126e7c0be1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 14:27:50 -0300

    feat: add RedirectUrlUseCase to UrlController for enhanced URL redirection

--- src/presentation/modules/url.module.ts
+++ src/presentation/modules/url.module.ts
@@ -1,5 +1,6 @@
 import { UrlController } from '../controller/url.controller';
 import { CreateUrlUseCase } from '@/application/useCases/CreateUrl.useCase';
+import { RedirectUrlUseCase } from '@/application/useCases/RedirectUrl.useCase';
 import { IUrlRepository } from '@/domain/repositories/IUrlRepository';
 import { CacheGateway } from '@/domain/interfaces/CacheGateway';
 import { UrlRepository } from '@/infra/repositories/url/UrlRepository.prisma';
@@ -9,21 +10,14 @@ import { FastifyInstance } from 'fastify';
 import { UrlCacheService } from '@/application/services/UrlCacheService';
 import { envProvider } from '@/infra/config/ProcessEnvProvider';
 
-// Importe ou instancie as dependências reais conforme sua arquitetura
-const urlRepository: IUrlRepository = new UrlRepository(
-  prisma
-) 
-
+const urlRepository: IUrlRepository = new UrlRepository(prisma);
 
 export function makeUrlController(app: FastifyInstance): UrlController {
-  const cache: CacheGateway = new RedisCacheGateway(app.redis)
+  const cache: CacheGateway = new RedisCacheGateway(app.redis);
   const urlCacheService = new UrlCacheService(cache);
 
-  const createUrlUseCase = new CreateUrlUseCase(
-    urlRepository, 
-    urlCacheService, 
-    envProvider
-  );
+  const createUrlUseCase = new CreateUrlUseCase(urlRepository, urlCacheService, envProvider);
+  const redirectUrlUseCase = new RedirectUrlUseCase(urlRepository, urlCacheService);
 
-  return new UrlController(createUrlUseCase);
+  return new UrlController(createUrlUseCase, redirectUrlUseCase);
 }
