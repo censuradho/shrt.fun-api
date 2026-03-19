9042564f44f0a9806b96ad5b3b25becfc094732b
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 15:18:40 -0300

    refactor(IUrlCacheService): update setUrl and getUrl methods to include urlId for improved URL management

--- src/domain/interfaces/IUrlCacheService.ts
+++ src/domain/interfaces/IUrlCacheService.ts
@@ -1,7 +1,7 @@
 export interface IUrlCacheService {
   incrementTotalUrls(): Promise<void>
   getTotalUrls(): Promise<number | null>
-  setUrl(shortUrl: string, originalUrl: string): Promise<void>
-  getUrl(shortUrl: string): Promise<string | null>
+  setUrl(shortUrl: string, originalUrl: string, urlId: string): Promise<void>
+  getUrl(shortUrl: string): Promise<{ originalUrl: string; urlId: string } | null>
   incrementHits(shortUrl: string): Promise<void>
 }
