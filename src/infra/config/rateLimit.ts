1404e8877086b1d15ec293f70fd30b9448f05423
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 14:01:04 -0300

    feat: add rate limit configuration for Fastify

---
+++ src/infra/config/rateLimit.ts
@@ -0,0 +1,14 @@
+import { RateLimitPluginOptions } from "@fastify/rate-limit";
+import { envProvider } from "./ProcessEnvProvider";
+
+export const rateLimitConfig: RateLimitPluginOptions = {
+  max: 20,
+  timeWindow: '1 minute',
+  global: true,
+  ban: 2,
+  nameSpace: 'rate-limiter',
+  ...(envProvider.get('NODE_ENV') === 'development' && ({
+    allowList: ['127.0.0.1'],
+    ban: undefined,
+  })),
+}
