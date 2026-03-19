00652fa409f3616b8d91f23936808195afb5ab19
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 12:15:04 -0300

    feat: add generateHash function for random string generation

---
+++ src/shered/generateHash.ts
@@ -0,0 +1,8 @@
+export function generateHash(length = 8): string {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let hash = '';
+  for (let i = 0; i < length; i++) {
+    hash += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return hash;
+}
