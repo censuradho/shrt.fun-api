2ebc35fdca4c32716e207ec38fca527e099a23d5
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 14:27:34 -0300

    feat: add redirect method to UrlController for handling URL redirection

--- src/presentation/controller/url.controller.ts
+++ src/presentation/controller/url.controller.ts
@@ -1,17 +1,23 @@
 import { CreateUrlUseCase } from "@/application/useCases/CreateUrl.useCase";
+import { RedirectUrlUseCase } from "@/application/useCases/RedirectUrl.useCase";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { CreateUrlDto } from "../dtos/url/createUrl.dto";
+import { HTTP_REDIRECT_CODES } from "@/shered/httpStatusCodes";
 
 export class UrlController {
   constructor (
-    private readonly createUrlUseCase: CreateUrlUseCase
+    private readonly createUrlUseCase: CreateUrlUseCase,
+    private readonly redirectUrlUseCase: RedirectUrlUseCase
   ) {}
 
   async create (request: FastifyRequest, reply: FastifyReply) {
     const payload = request.body as CreateUrlDto
-
     await this.createUrlUseCase.execute(payload)
-
     return reply.status(201).send()
+  }
+
+  async redirect (request: FastifyRequest<{ Params: { slug: string } }>, reply: FastifyReply) {
+    const originalUrl = await this.redirectUrlUseCase.execute(request.params.slug)
+    return reply.redirect(originalUrl, HTTP_REDIRECT_CODES.FOUND)
   }
 }
