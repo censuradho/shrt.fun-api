b88edcb6878b0d73fa77b5ec89e9efccbd7d206b
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 14:44:17 -0300

    feat: add HitRepository to UrlUnitOfWork for enhanced transaction handling

--- src/infra/repositories/url/UrlUnitOfWork.ts
+++ src/infra/repositories/url/UrlUnitOfWork.ts
@@ -2,9 +2,12 @@ import { IUnitOfWork } from "@/domain/interfaces/IUnitOfWork";
 import { IUrlRepository } from "@/domain/repositories/IUrlRepository";
 import { PrismaClient } from "prisma/generated/client";
 import { UrlRepository } from "./UrlRepository.prisma";
+import { IHitRepository } from "@/domain/repositories/IHitRepository";
+import { HitRepository } from "../hit/HitRepository";
 
 export interface IUrlUoWRepositories {
   url: IUrlRepository;
+  hit: IHitRepository
 }
 
 export type IUrlUnitOfWork = IUnitOfWork<IUrlUoWRepositories>;
@@ -17,7 +20,8 @@ export class UrlUnitOfWork implements IUrlUnitOfWork {
       const txClient = tx as unknown as PrismaClient;
 
       return work({ 
-        url: new UrlRepository(txClient) 
+        url: new UrlRepository(txClient),
+        hit: new HitRepository(txClient)
       });
     });
   }
