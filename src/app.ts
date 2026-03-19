cc19df4793060591fe692bae93faa0336bc8a72a
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 14:01:18 -0300

    feat: integrate rate limiting and error handling in Fastify app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import fastifyCors from "@fastify/cors";
 import Fastify from "fastify";
+import fastifyRateLimit from "@fastify/rate-limit";
 
 import { 
   ZodTypeProvider, 
@@ -12,6 +13,9 @@ import { corsConfig } from './infra/config/cors';
 import { redisPlugin } from './infra/http/plugins/redis';
 
 import { urlRoutes } from './presentation/routes/url.routes';
+import { rateLimitConfig } from './infra/config/rateLimit';
+import { HTTP_ERROR_CODES } from './shered/httpStatusCodes';
+import { errorHandler } from './presentation/middleware/error-handler';
 
 const app = Fastify({
   logger: process.env.NODE_ENV !== 'production' ? ({
@@ -26,9 +30,21 @@ const app = Fastify({
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+app.setErrorHandler(errorHandler);
 
 app.register(fastifyCors, corsConfig)
 app.register(redisPlugin)
+
+app.register(fastifyRateLimit, rateLimitConfig).after(() => {
+  app.setNotFoundHandler({
+    preHandler: app.rateLimit({
+      max: 4,
+      timeWindow: 500
+    })
+  }, function (_, reply) {
+    reply.status(HTTP_ERROR_CODES.NOT_FOUND).send({ error: 'Route not found' });
+  })
+})
 
 app.register(urlRoutes, { prefix: '/v1' });
 
