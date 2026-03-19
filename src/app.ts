83f397a455067be5a89f9f7ef4900354cdfcb5bd
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 09:38:17 -0300

    feat: initialize Fastify application with CORS and Redis plugins

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import fastifyCors from "@fastify/cors";
+import Fastify from "fastify";
+
+import { 
+  ZodTypeProvider, 
+  validatorCompiler, 
+  serializerCompiler, 
+} from "fastify-type-provider-zod";
+
+import { corsConfig } from './infra/config/cors';
+import { redisPlugin } from './infra/http/plugins/redis';
+
+const app = Fastify({
+  logger: process.env.NODE_ENV !== 'production' ? ({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true
+      }
+    }
+  }) : true
+}).withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, corsConfig)
+app.register(redisPlugin)
