0585f1021122111f2601770bcf6d747fcb0a52e4
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 14:01:14 -0300

    feat: add schema validation for URL creation endpoint

--- src/presentation/routes/url.routes.ts
+++ src/presentation/routes/url.routes.ts
@@ -1,8 +1,17 @@
 import { FastifyInstance } from 'fastify';
 import { makeUrlController } from '../modules/url.module';
+import { createUrlDto } from '../dtos/url/createUrl.dto';
 
 export async function urlRoutes(app: FastifyInstance) {
   const urlController = makeUrlController(app);
 
-  app.post('/url', urlController.create.bind(urlController));
+  app.post(
+    '/url', 
+    {
+      schema: {
+        body: createUrlDto
+      }
+    },  
+    urlController.create.bind(urlController)
+  );
 }
