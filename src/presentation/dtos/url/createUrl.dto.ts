5ac6d9ea5cc619b56dfcee153df5d35ec057f673
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 16:10:48 -0300

    feat(createUrlDto): enhance expireAt field validation to ensure future dates

--- src/presentation/dtos/url/createUrl.dto.ts
+++ src/presentation/dtos/url/createUrl.dto.ts
@@ -1,14 +1,24 @@
 import { FIELD_ERROR_MESSAGES } from "@/domain/errors/errors";
 import { sanitizeString } from "@/shered/sanitizeString";
+import { isAfter } from "date-fns";
 import z from "zod";
 
 export const createUrlDto = z.object({
-  url: z.url(FIELD_ERROR_MESSAGES.INVALID_FIELD),
+  url: z
+    .url(FIELD_ERROR_MESSAGES.INVALID_FIELD)
+    .max(2048, FIELD_ERROR_MESSAGES.MAX_LENGTH(2048)),
   slug: z
     .string()
+    .max(100, FIELD_ERROR_MESSAGES.MAX_LENGTH(100))
     .optional()
     .transform((value) => sanitizeString(value)),
-  expireAt: z.coerce.date().optional()
+  expireAt: z
+    .coerce
+    .date()
+    .optional()
+    .refine(date => !date || isAfter(date, new Date()), {
+      message: FIELD_ERROR_MESSAGES.DATE_MUST_BE_IN_FUTURE
+    })
 })
 
 export type CreateUrlDto = z.infer<typeof createUrlDto>
