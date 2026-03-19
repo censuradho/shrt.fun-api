7446135ce658c8ad4ed348025384416b1e7235e2
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-19 13:55:59 -0300

    feat: pass Fastify instance to makeUrlController for proper dependency injection

--- src/presentation/routes/url.routes.ts
+++ src/presentation/routes/url.routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { makeUrlController } from '../modules/url.module';
 
 export async function urlRoutes(app: FastifyInstance) {
-  const urlController = makeUrlController();
+  const urlController = makeUrlController(app);
 
   app.post('/url', urlController.create.bind(urlController));
 }
