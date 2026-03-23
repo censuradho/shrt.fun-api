cf22117fc94e0fc8fa3f6f49a2d5f13a6afb8fc7
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-23 14:01:43 -0300

    feat(createUrlDto): add URL and slug validation functions for improved input validation

--- src/presentation/dtos/url/createUrl.dto.ts
+++ src/presentation/dtos/url/createUrl.dto.ts
@@ -1,14 +1,17 @@
 import { FIELD_ERROR_MESSAGES } from "@/domain/errors/errors";
 import { sanitizeString } from "@/shered/sanitizeString";
+import { slugValidation, urlValidation } from "@/shered/validations";
 import { isAfter } from "date-fns";
 import z from "zod";
 
 export const createUrlDto = z.object({
   url: z
     .url(FIELD_ERROR_MESSAGES.INVALID_FIELD)
+    .refine(url => urlValidation(url), FIELD_ERROR_MESSAGES.INVALID_FIELD)
     .max(2048, FIELD_ERROR_MESSAGES.MAX_LENGTH(2048)),
   slug: z
     .string()
+    .refine(slug => slugValidation(slug), FIELD_ERROR_MESSAGES.INVALID_FIELD)
     .max(100, FIELD_ERROR_MESSAGES.MAX_LENGTH(100))
     .optional()
     .transform((value) => sanitizeString(value)),
