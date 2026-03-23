9e4a60eb6914c0725b5e196a75d2c878e293a082
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-23 19:00:28 -0300

    refactor(createUrlDto): remove expireAt field and related validations

--- src/presentation/dtos/url/createUrl.dto.ts
+++ src/presentation/dtos/url/createUrl.dto.ts
@@ -1,7 +1,6 @@
 import { FIELD_ERROR_MESSAGES } from "@/domain/errors/errors";
 import { sanitizeString } from "@/shered/sanitizeString";
 import { slugValidation, urlValidation } from "@/shered/validations";
-import { isAfter } from "date-fns";
 import z from "zod";
 
 export const createUrlDto = z.object({
@@ -15,13 +14,6 @@ export const createUrlDto = z.object({
     .max(100, FIELD_ERROR_MESSAGES.MAX_LENGTH(100))
     .optional()
     .transform((value) => sanitizeString(value)),
-  expireAt: z
-    .coerce
-    .date()
-    .optional()
-    .refine(date => !date || isAfter(date, new Date()), {
-      message: FIELD_ERROR_MESSAGES.DATE_MUST_BE_IN_FUTURE
-    })
 })
 
 export type CreateUrlDto = z.infer<typeof createUrlDto>
