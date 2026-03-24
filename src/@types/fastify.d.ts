486112be827fa037c5cbd0f68abb0d25052d73f1
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 18:43:17 -0300

    feat(types): extend FastifyRequest to include optional localUser property

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -1,7 +1,11 @@
 import 'fastify'
 import Redis from "ioredis";
+import { CookieUser } from '@/domain/interfaces/CookieUser';
 
 declare module 'fastify' {
+  interface FastifyRequest {
+    localUser?: CookieUser
+  }
   interface FastifyInstance {
     redis: Redis;
   }
