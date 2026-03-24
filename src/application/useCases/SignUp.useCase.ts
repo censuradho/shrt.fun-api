f1fc63e64f5b1f52187e7c63bf4be00f866ea929
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 10:02:47 -0300

    feat(useCases): implement SignUpUseCase for user registration logic

---
+++ src/application/useCases/SignUp.useCase.ts
@@ -0,0 +1,34 @@
+import { IUserRepository } from '@/domain/repositories/IUserRepository';
+import { AuthGateway } from './../../domain/interfaces/AuthGateway';
+import { SignUpDto } from "@/presentation/dtos/authentication/signUp.dto";
+import { AppError } from '@/domain/errors/AppError';
+import { USER_ERRORS_MESSAGES } from '@/domain/errors/user.errors';
+import { HTTP_STATUS_CODES } from '@/shered/httpStatusCodes';
+
+export class SignUpUseCase {
+  constructor (
+    private readonly authGateway: AuthGateway,
+    private readonly userRepository: IUserRepository
+  ) {}
+
+  async execute (input: SignUpDto) {
+    const { email, password, firstName, lastName } = input;
+
+    const userExists = await this.userRepository.checkIfExistsByEmail(email)
+
+    if (userExists) throw new AppError(USER_ERRORS_MESSAGES.USER_ALREADY_EXISTS, {
+      status: HTTP_STATUS_CODES.CONFLICT
+    })
+
+    const userId = await this.userRepository.create({ email, firstName, lastName })
+
+    try {
+      await this.authGateway.signUpWithEmailAndPassword({ email, password })
+    } catch  {
+      await this.userRepository.delete(userId)
+      throw new AppError(USER_ERRORS_MESSAGES.SIGN_UP_FAILED, {
+        status: HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
+      })
+    }
+  }
+}
