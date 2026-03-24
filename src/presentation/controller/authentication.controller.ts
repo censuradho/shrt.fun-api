a23a05cbd5d567d4909761846edcff1f088af17c
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 15:37:03 -0300

    fix(auth): handle AppError in signUpWithEmailAndPassword method

--- src/presentation/controller/authentication.controller.ts
+++ src/presentation/controller/authentication.controller.ts
@@ -1,6 +1,7 @@
 import { SignUpWithEmailAndPasswordUseCase } from "@/application/useCases/SignUpWithEmailAndPassword.useCase";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { SignUpDto } from "../dtos/authentication/signUp.dto";
+import { AppError } from "@/domain/errors/AppError";
 
 export class AuthenticationController {
   constructor(
@@ -8,15 +9,23 @@ export class AuthenticationController {
   ) {}
 
   async signUpWithEmailAndPassword(request: FastifyRequest, reply: FastifyReply) {
-    const body = request.body as SignUpDto;
+    try {
+      const body = request.body as SignUpDto;
 
-    await this.signUpWithEmailAndPasswordUseCase.execute({
-      email: body.email,
-      password: body.password,
-      firstName: body.firstName,
-      lastName: body.lastName,
-    });
+      await this.signUpWithEmailAndPasswordUseCase.execute({
+        email: body.email,
+        password: body.password,
+        firstName: body.firstName,
+        lastName: body.lastName,
+      });
 
-    return reply.status(201).send();
+      return reply.status(200).send();
+    } catch (error: unknown) {
+      if (error instanceof AppError) {
+        return reply.status(200).send()
+      }
+
+      throw error
+    }
   }
 }
