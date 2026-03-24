9608cba2c167a76b331fc8648cf2bb2f4dc34e40
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 10:18:28 -0300

    feat(auth): add supabaseClient for authentication integration

---
+++ src/infra/auth/client.ts
@@ -0,0 +1,8 @@
+import { envProvider } from "../config/ProcessEnvProvider";
+import { SupabaseClientFactory } from "./SupabaseClientFactory";
+
+export const supabaseClient = SupabaseClientFactory
+  .build({
+    url: envProvider.get('SUPABASE_URL') || '',
+    serviceRoleKey: envProvider.get('SUPABASE_SERVICE_ROLE_KEY') || ''
+  })
