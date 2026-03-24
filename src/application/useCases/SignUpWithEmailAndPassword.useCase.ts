dc6e15f670e9fbe4b353fcde3fb824ca0ba64f93
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 10:18:22 -0300

    feat(useCases): add SignUpWithEmailAndPasswordUseCase for user registration

--- src/application/useCases/SignUpWithEmailAndPassword.useCase.ts
+++ src/application/useCases/SignUpWithEmailAndPassword.useCase.ts
@@ -1,14 +1,17 @@
 import { IUserRepository } from '@/domain/repositories/IUserRepository';
-import { AuthGateway } from './../../domain/interfaces/AuthGateway';
+import { AuthGateway } from '../../domain/interfaces/AuthGateway';
 import { SignUpDto } from "@/presentation/dtos/authentication/signUp.dto";
 import { AppError } from '@/domain/errors/AppError';
 import { USER_ERRORS_MESSAGES } from '@/domain/errors/user.errors';
 import { HTTP_STATUS_CODES } from '@/shered/httpStatusCodes';
+import { ErrorLog } from '@/shered/ErrorLog';
+import { FastifyInstance } from 'fastify';
 
-export class SignUpUseCase {
+export class SignUpWithEmailAndPasswordUseCase {
   constructor (
     private readonly authGateway: AuthGateway,
-    private readonly userRepository: IUserRepository
+    private readonly userRepository: IUserRepository,
+    private readonly app: FastifyInstance
   ) {}
 
   async execute (input: SignUpDto) {
@@ -24,8 +27,12 @@ export class SignUpUseCase {
 
     try {
       await this.authGateway.signUpWithEmailAndPassword({ email, password })
-    } catch  {
+    } catch(error: any)  {
       await this.userRepository.delete(userId)
+      this.app.log.error(
+        new ErrorLog({ err: error, context: 'SignUpWithEmailAndPasswordUseCase.execute', input: { email } }),
+      )
+
       throw new AppError(USER_ERRORS_MESSAGES.SIGN_UP_FAILED, {
         status: HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
       })
