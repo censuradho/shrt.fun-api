0daea9a5dc03f0fdeb6e4635ae952b601e44c70c
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-24 10:02:43 -0300

    feat(dtos): create signUpDto for user registration validation

---
+++ src/presentation/dtos/authentication/signUp.dto.ts
@@ -0,0 +1,15 @@
+import { AUTHENTICATION_ERROR_MESSAGES } from "@/domain/errors/authentication.errors";
+import { FIELD_ERROR_MESSAGES } from "@/domain/errors/errors";
+import z from "zod";
+
+export const signUpDto = z.object({
+  email: z.email(FIELD_ERROR_MESSAGES.INVALID_FIELD).max(255, FIELD_ERROR_MESSAGES.MAX_LENGTH(255)),
+  password: z
+    .string()
+    .min(8, AUTHENTICATION_ERROR_MESSAGES.PASSWORD_MIN_8_CHARACTERS)
+    .max(256, FIELD_ERROR_MESSAGES.MAX_LENGTH(256)),
+  firstName: z.string().min(1, FIELD_ERROR_MESSAGES.REQUIRED).max(255, FIELD_ERROR_MESSAGES.MAX_LENGTH(255)),
+  lastName: z.string().min(1, FIELD_ERROR_MESSAGES.REQUIRED).max(255, FIELD_ERROR_MESSAGES.MAX_LENGTH(255)),
+})
+
+export type SignUpDto = z.infer<typeof signUpDto>
