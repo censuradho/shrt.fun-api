5d3382a33622ad977f3af871e980adc77a74903d
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 14:15:47 -0300

    feat(routes): register private URL routes in the main routes file

--- src/presentation/routes/routes.ts
+++ src/presentation/routes/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { authRoutes } from "./auth.routes";
+import { urlRoutesPrivate } from "./url.private.routes";
 
 export function routes (app: FastifyInstance) {
   app.register(authRoutes, { prefix: '/auth' })
+  app.register(urlRoutesPrivate, { prefix: '/url' })
 }
