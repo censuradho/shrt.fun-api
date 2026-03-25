d9214f86b396ae1d66c3730a1e280e2731bb9a46
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 14:15:15 -0300

    feat(controller): add findManyPaginated method for retrieving paginated links

--- src/presentation/controller/url.controller.ts
+++ src/presentation/controller/url.controller.ts
@@ -3,11 +3,14 @@ import { RedirectUrlUseCase } from "@/application/useCases/RedirectUrl.useCase";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { CreateUrlDto } from "../dtos/url/createUrl.dto";
 import { HTTP_REDIRECT_CODES } from "@/shered/httpStatusCodes";
+import { FindManyLinksFiltersDto } from "../dtos/url/findManyLinksQueries.dto";
+import { FindManyLinksPaginatedQuery } from "@/application/queries/findManyLinksPaginated.query";
 
 export class UrlController {
   constructor (
     private readonly createUrlUseCase: CreateUrlUseCase,
-    private readonly redirectUrlUseCase: RedirectUrlUseCase
+    private readonly redirectUrlUseCase: RedirectUrlUseCase,
+    private readonly findManyLinksPaginatedQuery: FindManyLinksPaginatedQuery
   ) {}
 
   async create (request: FastifyRequest, reply: FastifyReply) {
@@ -24,5 +27,13 @@ export class UrlController {
       request.headers['user-agent'] || ''
     );
     return reply.redirect(originalUrl, HTTP_REDIRECT_CODES.FOUND)
+  }
+
+  async findManyPaginated (request: FastifyRequest, reply: FastifyReply) {
+    const queries = request.query as FindManyLinksFiltersDto
+
+    const result = await this.findManyLinksPaginatedQuery.execute(request.user.id, queries)
+
+    return reply.send(result);
   }
 }
