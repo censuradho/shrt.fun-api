801708496b4817aa8899213108705fc8bf6d207a
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 14:15:41 -0300

    feat(controller): integrate FindManyLinksPaginatedQuery into UrlController

--- src/presentation/modules/url.module.ts
+++ src/presentation/modules/url.module.ts
@@ -10,11 +10,13 @@ import { UrlRepository } from '@/infra/repositories/url/UrlRepository.prisma';
 import { UrlUnitOfWork } from '@/infra/repositories/url/UrlUnitOfWork';
 import { FastifyInstance } from 'fastify';
 import { UrlController } from '../controller/url.controller';
+import { FindManyLinksPaginatedQuery } from '@/application/queries/findManyLinksPaginated.query';
 
 const urlRepository: IUrlRepository = new UrlRepository(prisma);
 
 const urlUnitOfWork = new UrlUnitOfWork(prisma);
-
+const findManyLinksPaginatedQuery = new FindManyLinksPaginatedQuery(urlRepository);
+  
 export function makeUrlController(app: FastifyInstance): UrlController {
   const cache: CacheGateway = new RedisCacheGateway(app.redis);
   const urlCacheService = new UrlCacheService(cache);
@@ -25,5 +27,9 @@ export function makeUrlController(app: FastifyInstance): UrlController {
     urlUnitOfWork
   );
 
-  return new UrlController(createUrlUseCase, redirectUrlUseCase);
+  return new UrlController(
+    createUrlUseCase, 
+    redirectUrlUseCase, 
+    findManyLinksPaginatedQuery
+  );
 }
