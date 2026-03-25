a1fee88c6fb34bbc7f36b3d86c9d95872ce527ea
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 15:09:09 -0300

    feat(useCases): implement CreateShortUrlUseCase for generating short URLs

---
+++ src/application/useCases/CreateShortUrl.useCase.ts
@@ -0,0 +1,42 @@
+import { AppError } from "@/domain/errors/AppError";
+import { URL_ERRORS } from "@/domain/errors/url.error";
+import { IShortUrlGenerateService } from "@/domain/interfaces/IShortUrlGenerateService";
+import { IUrlCacheService } from "@/domain/interfaces/IUrlCacheService";
+import { IUrlRepository } from '@/domain/repositories/IUrlRepository';
+import { CreateUrlDto } from "@/presentation/dtos/url/createUrl.dto";
+import { HTTP_ERROR_CODES } from "@/shered/httpStatusCodes";
+
+export class CreateShortUrlUseCase {
+  constructor (
+    private readonly urlRepository: IUrlRepository,
+    private readonly urlCacheService: IUrlCacheService,
+    private readonly shortUrlGenerateService: IShortUrlGenerateService
+  ) {}
+
+  async execute(userId: string, dto: CreateUrlDto): Promise<string> {
+    const { url, slug } = dto;
+
+    const shortUrl = await this.shortUrlGenerateService.generate(slug);
+
+    const urlId = await this.urlRepository.create(
+      userId,
+      {
+        originalUrl: url,
+        shortUrl,
+      });
+
+    try {
+      await Promise.all([
+        this.urlCacheService.setUrl(shortUrl, url, urlId, 60 * 60 * 24 * 30),
+        this.urlCacheService.incrementTotalUrls(),
+      ])
+
+      return shortUrl;
+    } catch {
+      await this.urlRepository.delete(urlId);
+      throw new AppError(URL_ERRORS.WAS_NOT_POSSIBLE_TO_CREATE_SHORT_URL, {
+        status: HTTP_ERROR_CODES.INTERNAL_SERVER_ERROR
+      });
+    }
+  }
+}
