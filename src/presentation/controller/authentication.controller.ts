112173120348bda33e178226c9d4bf3865976529
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 10:27:12 -0300

    feat(auth): add me method to retrieve user by Supabase ID

--- src/presentation/controller/authentication.controller.ts
+++ src/presentation/controller/authentication.controller.ts
@@ -2,10 +2,12 @@ import { SignUpWithEmailAndPasswordUseCase } from "@/application/useCases/SignUp
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { SignUpDto } from "../dtos/authentication/signUp.dto";
 import { AppError } from "@/domain/errors/AppError";
+import { IUserRepository } from "@/domain/repositories/IUserRepository";
 
 export class AuthenticationController {
   constructor(
     private readonly signUpWithEmailAndPasswordUseCase: SignUpWithEmailAndPasswordUseCase,
+    private readonly userRepository: IUserRepository
   ) {}
 
   async signUpWithEmailAndPassword(request: FastifyRequest, reply: FastifyReply) {
@@ -27,5 +29,13 @@ export class AuthenticationController {
 
       throw error
     }
+  }
+
+  async me (request: FastifyRequest, reply: FastifyReply) {
+    const supabaseId = request.user.id
+
+    const user = await this.userRepository.findUserBySupabaseId(supabaseId)
+
+    return reply.send(user)
   }
 }
