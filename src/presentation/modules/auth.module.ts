bb4794c304a56ed6eda0276b6100c2df63ede809
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 11:19:25 -0300

    feat(auth): add PlanRepository to authentication controller for user signup use case

--- src/presentation/modules/auth.module.ts
+++ src/presentation/modules/auth.module.ts
@@ -4,16 +4,19 @@ import { SignUpWithEmailAndPasswordUseCase } from "@/application/useCases/SignUp
 import { SupabaseAuthGateway } from "@/infra/auth/SupabaseAuthGateway";
 import { supabaseClient } from "@/infra/auth/client";
 import { UserRepository } from "@/infra/repositories/user/UserRepository.prisma";
+import { PlanRepository } from "@/infra/repositories/plan/PlanRepository.prisma";
 import { prisma } from "@/infra/database/prisma";
 import { envProvider } from "@/infra/config/ProcessEnvProvider";
 
 export function makeAuthController(app: FastifyInstance): AuthenticationController {
   const supabaseAuthGateway = new SupabaseAuthGateway(supabaseClient, envProvider)
   const userRepository = new UserRepository(prisma)
+  const planRepository = new PlanRepository(prisma)
 
   const signUpWithEmailAndPasswordUseCase = new SignUpWithEmailAndPasswordUseCase(
     supabaseAuthGateway,
     userRepository,
+    planRepository,
     app
   )
 
