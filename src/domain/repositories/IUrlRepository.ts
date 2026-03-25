a150dd319037bd04f3fc84f53c5efa7d9250fcef
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 14:14:48 -0300

    feat(repository): implement pagination in UrlRepository for retrieving URLs

--- src/domain/repositories/IUrlRepository.ts
+++ src/domain/repositories/IUrlRepository.ts
@@ -1,13 +1,18 @@
+import { UrlModel } from "prisma/generated/models";
+import { PaginationParams, PaginationResult } from "../interfaces/Pagination";
 import { CreateUrlEntityDto } from "./dtos/CreateUrlEntity.dto";
+
+export interface UrlPaginationFilters {
+  isActive?: boolean
+}
 
 export interface IUrlRepository {
   create (payload: CreateUrlEntityDto): Promise<string>
-
   getIdByShortUrl (url: string): Promise<string | null>
-
   getOriginalUrl (shortUrl: string): Promise<{ id: string; originalUrl: string } | null>
-
   incrementHitsCount (id: string): Promise<void>
-
   delete (id: string): Promise<void>
+  findManyPaginated (userId: string, pagination: PaginationParams, filters: UrlPaginationFilters): Promise<
+    PaginationResult<UrlModel>
+  >
 }
--- src/infra/repositories/url/UrlRepository.prisma.ts
+++ src/infra/repositories/url/UrlRepository.prisma.ts
@@ -1,10 +1,44 @@
+import { PaginationParams, PaginationResult } from "@/domain/interfaces/Pagination";
 import { CreateUrlEntityDto } from "@/domain/repositories/dtos/CreateUrlEntity.dto";
-import { IUrlRepository } from "@/domain/repositories/IUrlRepository";
+import { IUrlRepository, UrlPaginationFilters } from "@/domain/repositories/IUrlRepository";
 import { nanoid } from "nanoid";
 import { PrismaClient } from "prisma/generated/client";
+import { UrlModel, UrlWhereInput } from "prisma/generated/models";
 
 export class UrlRepository implements IUrlRepository {
   constructor (private readonly prisma: PrismaClient) {}
+
+  async findManyPaginated(
+    userId: string, 
+    pagination: PaginationParams, 
+    filters: UrlPaginationFilters
+  ): Promise<PaginationResult<UrlModel>> {
+    const { cursor, limit } = pagination;
+    const { isActive } = filters
+
+    const where: UrlWhereInput = {
+      userId,
+      ...(isActive !== undefined && { isActive })
+    }
+
+    const data = await this.prisma.url.findMany({
+      where,
+      take: limit + 1,
+      ...(cursor && { skip: 1, cursor: { id: cursor } }),
+      orderBy: [
+        { createdAt: "desc" },
+        { id: "asc" },
+      ]
+    });
+
+    const hasNextPage = data.length > limit;
+    const items = hasNextPage ? data.slice(0, -1) : data;
+
+    return {
+      data: items,
+      nextCursor: hasNextPage ? (items.at(-1)?.id ?? null) : null,
+    };
+  }
 
   async getOriginalUrl(shortUrl: string): Promise<{ originalUrl: string; id: string } | null> {
     const data = await this.prisma.url.findUnique({
