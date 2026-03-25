5d6bd9d592f0a4cdf1a9765637e2c0166a7b111d
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-25 17:58:17 -0300

    refactor(routes): add POST route for URL creation with validation schema

--- src/presentation/routes/url.private.routes.ts
+++ src/presentation/routes/url.private.routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { makeUrlController } from '../modules/url.module';
 import { authMiddleware } from '../middleware/auth.middleware';
 import { findManyLinksFiltersDto } from '../dtos/url/findManyLinksQueries.dto';
+import { createUrlDto } from '../dtos/url/createUrl.dto';
 
 export async function urlRoutesPrivate(app: FastifyInstance) {
   const urlController = makeUrlController(app);
@@ -15,5 +16,16 @@ export async function urlRoutesPrivate(app: FastifyInstance) {
       },
     },
     urlController.findManyPaginated.bind(urlController)
+  );
+
+  app.post(
+    '/',
+    {
+      preHandler: [authMiddleware],
+      schema: {
+        body: createUrlDto
+      }
+    },
+    urlController.create.bind(urlController)
   );
 }
