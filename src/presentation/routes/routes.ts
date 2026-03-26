68596814d4aff664292d775924eec0a1a632c937
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-26 13:31:18 -0300

    feat(routes): register private analytics routes with prefix '/analytics'

--- src/presentation/routes/routes.ts
+++ src/presentation/routes/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { authRoutes } from "./auth.routes";
 import { urlRoutesPrivate } from "./url.private.routes";
+import { analyticsRoutesPrivate } from "./analytics.private.routes";
 
 export function routes (app: FastifyInstance) {
   app.register(authRoutes, { prefix: '/auth' })
   app.register(urlRoutesPrivate, { prefix: '/url' })
+  app.register(analyticsRoutesPrivate, { prefix: '/analytics' })
 }
