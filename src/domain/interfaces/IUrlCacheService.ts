47364e9e60e7d94d63fe6887548e7093251bf517
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-26 00:10:08 -0300

    refactor(IUrlCacheService): add isActive parameter to setUrl and getUrl methods

--- src/domain/interfaces/IUrlCacheService.ts
+++ src/domain/interfaces/IUrlCacheService.ts
@@ -1,7 +1,7 @@
 export interface IUrlCacheService {
   incrementTotalUrls(ttl?: number): Promise<void>
   getTotalUrls(): Promise<number | null>
-  setUrl(shortUrl: string, originalUrl: string, urlId: string, ttl?: number): Promise<void>
-  getUrl(shortUrl: string): Promise<{ originalUrl: string; urlId: string } | null>
+  setUrl(shortUrl: string, originalUrl: string, urlId: string, isActive: boolean, ttl?: number): Promise<void>
+  getUrl(shortUrl: string): Promise<{ originalUrl: string; urlId: string; isActive: boolean } | null>
   incrementHits(shortUrl: string, ttl?: number): Promise<void>
 }
