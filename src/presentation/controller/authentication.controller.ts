1af89fd900eae013e4e4d758b07a54b8bf8018f7
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-26 18:04:50 -0300

    feat(auth): enhance error logging in signUpWithEmailAndPassword method

--- src/presentation/controller/authentication.controller.ts
+++ src/presentation/controller/authentication.controller.ts
@@ -1,18 +1,20 @@
 import { SignUpWithEmailAndPasswordUseCase } from "@/application/useCases/SignUpWithEmailAndPassword.useCase";
-import type { FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { SignUpDto } from "../dtos/authentication/signUp.dto";
 import { AppError } from "@/domain/errors/AppError";
 import { IUserRepository } from "@/domain/repositories/IUserRepository";
+import { ErrorLog } from "@/shered/ErrorLog";
 
 export class AuthenticationController {
   constructor(
     private readonly signUpWithEmailAndPasswordUseCase: SignUpWithEmailAndPasswordUseCase,
-    private readonly userRepository: IUserRepository
+    private readonly userRepository: IUserRepository,
+    private readonly app: FastifyInstance
   ) {}
 
   async signUpWithEmailAndPassword(request: FastifyRequest, reply: FastifyReply) {
+    const body = request.body as SignUpDto;
     try {
-      const body = request.body as SignUpDto;
 
       await this.signUpWithEmailAndPasswordUseCase.execute({
         email: body.email,
@@ -23,6 +25,14 @@ export class AuthenticationController {
 
       return reply.status(200).send();
     } catch (error: unknown) {
+      
+      this.app.log.error(
+        new ErrorLog({ 
+          err: error, 
+          context: 'AuthenticationController.signUpWithEmailAndPassword', 
+          input: { email: body.email } 
+        }),
+      )
       if (error instanceof AppError) {
         return reply.status(200).send()
       }
