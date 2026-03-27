c70259753e149c00a7087e88e4be2e2aad305f34
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-27 18:36:37 -0300

    feat(pagination): add OffsetPaginationParams and OffsetPaginationResult interfaces

--- src/domain/interfaces/Pagination.ts
+++ src/domain/interfaces/Pagination.ts
@@ -7,3 +7,15 @@ export interface CursorPaginationResult<T> {
   data: T[]
   nextCursor: string | null
 }
+
+export interface OffsetPaginationParams {
+  limit: number
+  offset: number
+}
+
+export interface OffsetPaginationResult<T> {
+  data: T[]
+  total: number
+  limit: number
+  offset: number
+}
