687b59ff4061d4048b5bc3bbbbf1617691265d98
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-27 13:13:24 -0300

    feat(dtos): add offsetPaginationQueriesDto

--- src/presentation/dtos/paginationQueries.dto.ts
+++ src/presentation/dtos/paginationQueries.dto.ts
@@ -10,3 +10,9 @@ export const cursorPaginationQueriesDto = z.object({
 
 export type CursorPaginationQueriesDto = z.infer<typeof cursorPaginationQueriesDto>;
 
+export const offsetPaginationQueriesDto = z.object({
+  offset: z.coerce.number().int().nonnegative().optional().default(0),
+  limit: z.coerce.number().int().positive().max(100).optional().default(10),
+});
+
+export type OffsetPaginationQueriesDto = z.infer<typeof offsetPaginationQueriesDto>;
