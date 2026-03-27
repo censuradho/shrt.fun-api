9b30bc2cc26b05349a4f5c54e0a2f935269869d8
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-27 18:37:06 -0300

    feat(routes): update analytics routes to include countries and cities endpoints with pagination

--- src/presentation/routes/analytics.private.routes.ts
+++ src/presentation/routes/analytics.private.routes.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from '@/presentation/middleware/auth.middleware';
+import { offsetPaginationQueriesDto } from '@/presentation/dtos/paginationQueries.dto';
 import { FastifyInstance } from 'fastify';
 import z from 'zod';
 import { makeAnalyticsController } from '../modules/analytics.module';
@@ -9,11 +10,29 @@ export async function analyticsRoutesPrivate(app: FastifyInstance) {
   const analyticsController = makeAnalyticsController();
 
   app.get(
-    '/hits/url/:urlId/locations',
+    '/locations/hits/url/:urlId/',
     {
       preHandler: authMiddleware,
       schema: { params: urlParamsDto },
     },
     analyticsController.findLocations.bind(analyticsController)
+  );
+
+  app.get(
+    '/locations/hits/countries',
+    {
+      preHandler: authMiddleware,
+      schema: { querystring: offsetPaginationQueriesDto },
+    },
+    analyticsController.findCountriesByUserId.bind(analyticsController)
+  );
+
+  app.get(
+    '/locations/hits/cities',
+    {
+      preHandler: authMiddleware,
+      schema: { querystring: offsetPaginationQueriesDto },
+    },
+    analyticsController.findCitiesByUserId.bind(analyticsController)
   );
 }
