7e3d522333e84b2d3b57aefda8b4866c5fab490c
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-30 20:47:02 -0300

    feat(CreateShortUrlUseCase): check if user has not reached the daily limit based on his plan

--- src/application/useCases/CreateShortUrl.useCase.ts
+++ src/application/useCases/CreateShortUrl.useCase.ts
@@ -3,40 +3,54 @@ import { URL_ERRORS } from "@/domain/errors/url.error";
 import { IShortUrlGenerateService } from "@/domain/interfaces/IShortUrlGenerateService";
 import { IUrlCacheService } from "@/domain/interfaces/IUrlCacheService";
 import { IUrlRepository } from '@/domain/repositories/IUrlRepository';
+import { IUserRepository } from "@/domain/repositories/IUserRepository";
 import { CreateUrlDto } from "@/presentation/dtos/url/createUrl.dto";
-import { HTTP_ERROR_CODES } from "@/shered/httpStatusCodes";
+import { HTTP_ERROR_CODES, HTTP_STATUS_CODES } from "@/shered/httpStatusCodes";
 
 export class CreateShortUrlUseCase {
   constructor (
     private readonly urlRepository: IUrlRepository,
     private readonly urlCacheService: IUrlCacheService,
-    private readonly shortUrlGenerateService: IShortUrlGenerateService
+    private readonly shortUrlGenerateService: IShortUrlGenerateService,
+    private readonly userRepository: IUserRepository,
   ) {}
 
-  async execute(userId: string, dto: CreateUrlDto): Promise<string> {
+  async execute(supabaseId: string, dto: CreateUrlDto): Promise<string> {
     const { url, slug, title } = dto;
+
+    const user = await this.userRepository.findUserBySupabaseId(supabaseId);
+
+    if (!user) throw new AppError(URL_ERRORS.WAS_NOT_POSSIBLE_TO_CREATE_SHORT_URL, {
+      status: HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR,
+    });
+
+    const count = await this.urlRepository.countByUserCurrentMonth(supabaseId);
+
+    if (count >= user.plan.monthlyLinkLimit) {
+      throw new AppError(URL_ERRORS.MONTHLY_LINK_LIMIT_REACHED, {
+        status: HTTP_STATUS_CODES.FORBIDDEN,
+      });
+    }
 
     const shortUrl = await this.shortUrlGenerateService.generate(slug);
 
-    const urlId = await this.urlRepository.create(
-      userId,
-      {
-        originalUrl: url,
-        shortUrl,
-        title,
-      });
+    const urlId = await this.urlRepository.create(supabaseId, {
+      originalUrl: url,
+      shortUrl,
+      title,
+    });
 
     try {
       await Promise.all([
         this.urlCacheService.setUrl(shortUrl, url, urlId, true, 60 * 60 * 24 * 30),
         this.urlCacheService.incrementTotalUrls(),
-      ])
+      ]);
 
       return shortUrl;
     } catch {
       await this.urlRepository.delete(urlId);
       throw new AppError(URL_ERRORS.WAS_NOT_POSSIBLE_TO_CREATE_SHORT_URL, {
-        status: HTTP_ERROR_CODES.INTERNAL_SERVER_ERROR
+        status: HTTP_ERROR_CODES.INTERNAL_SERVER_ERROR,
       });
     }
   }
