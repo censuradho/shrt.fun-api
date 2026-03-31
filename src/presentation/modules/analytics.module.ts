f59fce1dad139f9da80212730b16127a78813fb2
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-31 20:39:50 -0300

    feat(analytics): add analyticsRepository to makeAnalyticsController

--- src/presentation/modules/analytics.module.ts
+++ src/presentation/modules/analytics.module.ts
@@ -5,9 +5,11 @@ import { prisma } from '@/infra/database/prisma';
 import { HitRepository } from '@/infra/repositories/hit/HitRepository';
 import { UrlRepository } from '@/infra/repositories/url/UrlRepository.prisma';
 import { AnalyticsController } from '../controller/analytics.controller';
+import { AnalyticsRepository } from '@/infra/repositories/analytics/AnalyticsRepository.prisma';
 
 const urlRepository = new UrlRepository(prisma);
 const hitRepository = new HitRepository(prisma);
+const analyticsRepository = new AnalyticsRepository(prisma);
 
 export function makeAnalyticsController(): AnalyticsController {
   const findLocationAnalyticsQuery = new FindLocationAnalyticsQuery(urlRepository, hitRepository);
@@ -18,5 +20,6 @@ export function makeAnalyticsController(): AnalyticsController {
     findLocationAnalyticsQuery,
     findCountryAnalyticsByUserQuery,
     findCityAnalyticsByUserQuery,
+    analyticsRepository
   );
 }
