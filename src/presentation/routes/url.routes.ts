f414fe48530a6f94713d3792f1219dbe258d23f0
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-31 13:47:17 -0300

    refactor(url.routes): remove unused URL creation endpoint and related logic

--- src/presentation/routes/url.routes.ts
+++ src/presentation/routes/url.routes.ts
@@ -1,33 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import { makeUrlController } from '../modules/url.module';
-import { createUrlDto } from '../dtos/url/createUrl.dto';
-import { URL_ERRORS } from '@/domain/errors/url.error';
-import { AppError } from '@/domain/errors/AppError';
-import { HTTP_STATUS_CODES } from '@/shered/httpStatusCodes';
 
 export async function urlRoutesPublic(app: FastifyInstance) {
   const urlController = makeUrlController(app);
-
-  app.post(
-    '/url', 
-    {
-      schema: {
-        body: createUrlDto
-      },
-      config: {
-        rateLimit: {
-          max: 2,
-          timeWindow: '1 hour',
-          errorResponseBuilder: () => {
-            throw new AppError(URL_ERRORS.ONLY_1_URL_CREATIONS_PER_DAY, {
-              status: HTTP_STATUS_CODES.TOO_MANY_REQUESTS
-            })
-          }
-        },
-      }
-    },
-    urlController.createAnonymous.bind(urlController)
-  );
 
   app.get(
     '/:slug',
