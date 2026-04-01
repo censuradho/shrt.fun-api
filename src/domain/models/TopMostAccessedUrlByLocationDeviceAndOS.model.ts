8d4e59bcd549c9e792744de260b231028d06c8a7
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-31 21:43:47 -0300

    feat(models): add TopMostAccessedUrlByLocationDeviceAndOSModel interface

---
+++ src/domain/models/TopMostAccessedUrlByLocationDeviceAndOS.model.ts
@@ -0,0 +1,8 @@
+export interface TopMostAccessedUrlByLocationDeviceAndOSModel {
+  city?: string | null
+  country?: string | null
+  device?: string | null
+  os?: string | null
+  hitsCount: number
+  shortUrl: string
+}
