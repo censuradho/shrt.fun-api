13eed12d64abe93dfdce7e1e3755195c437056cd
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-03-31 21:44:01 -0300

    feat(dtos): remove limit field from topMostAccessedUrlsQueryDto

--- src/presentation/dtos/analytics.dto.ts
+++ src/presentation/dtos/analytics.dto.ts
@@ -1,8 +1,6 @@
-import { FIELD_ERROR_MESSAGES } from "@/domain/errors/errors";
 import z from "zod";
 
 export const topMostAccessedUrlsQueryDto = z.object({
-  limit: z.coerce.number().max(10, FIELD_ERROR_MESSAGES.MAX_VALUE(10)).optional(),
   isActive: z.enum(['true', 'false']).transform(v => v === 'true').optional(),
 })
 
