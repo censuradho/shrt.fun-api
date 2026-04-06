377831dde85490ec27312235a4200984168764d0
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-06 11:46:17 -0300

    fix(env): update environment variable retrieval to use env.dto

--- src/infra/config/ProcessEnvProvider.ts
+++ src/infra/config/ProcessEnvProvider.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 
 import { IEnvProvider, LocalEnvironmentVars } from "@/domain/EnvProvider";
+import { env } from "./env.dto";
 
 export class ProcessEnvProvider implements IEnvProvider {
   get(key: keyof LocalEnvironmentVars): string | undefined {
-    return process.env[key];
+    return env[key];
   }
 }
 
