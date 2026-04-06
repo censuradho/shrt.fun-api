0eec3db302b49bd32c05fc7f2f076e218c1ddd10
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-06 07:49:55 -0300

    fix(database): update PrismaPg adapter configuration to include SSL settings

--- src/infra/database/prisma.ts
+++ src/infra/database/prisma.ts
@@ -3,7 +3,10 @@ import { envProvider } from "../config/ProcessEnvProvider";
 import { PrismaClient } from "@/generated/prisma/client";
 
 
-const adapter = new PrismaPg({ connectionString: envProvider.get('DATABASE_URL') });
+const adapter = new PrismaPg({ 
+  connectionString: envProvider.get('DATABASE_URL'),
+  ssl: { rejectUnauthorized: false },
+});
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
